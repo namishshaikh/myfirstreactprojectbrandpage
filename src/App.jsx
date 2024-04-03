13900ad1ca84d64072ddd4208d96d80d57dac820
namishshaikh <namishshaikh008@gmail.com>
2024-04-04 00:41:50 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Herosection from './components/Hero';
+import Navigation from './components/Navigation'
+const App = () =>{
+ return (
+  <div>
+  <Navigation/>
+  <Herosection/>
+  </div>
+ );
+};
+
+export default App;
